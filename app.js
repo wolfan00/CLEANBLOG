4e1359c92c4285d5d469c7b1993f2a15dc597964
wolfan00 <burhanerkulu00@gmail.com>
2025-02-28 14:08:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
-app.get('/', (req, res) => res.send('Clean Blog'))
+app.get('/', (req, res) =>{
+    const blog = { id: 1, title: "Blog title", description: "Blog description" }
+    res.send(blog)
+}) 
 app.listen(port, () => console.log(`CleanBlog projesi ${port} dan dinleniyor!`))
