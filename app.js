3a8d3e2a183f023f3b1c8be7449ca1eefea42c40
wolfan00 <burhanerkulu00@gmail.com>
2025-02-28 17:39:32 +0300

    added ejs

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
+const ejs = require('ejs')
 const port = 5000
-
+//template engine
+app.set("view engine","ejs")
+//middle wares
+app.use(express.static(`public`))
+//routes
 app.get('/', (req, res) =>{
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(blog)
+    res.render(`index`)
 }) 
+app.get('/about', (req, res) =>{
+    res.render(`about`)
+}) 
+app.get('/post', (req, res) =>{
+    res.render(`post`)
+}) 
+app.get('/add_post', (req, res) =>{
+    res.render(`add_post`)
+}) 
+
 app.listen(port, () => console.log(`CleanBlog projesi ${port} dan dinleniyor!`))
